58dac0f111a063aef60ee9073c4636a69e47070a
Ulrich <ulrich@pop-os.localdomain>
2020-11-20 19:06:47 -0800

    bit wise logs

--- javaScript/chapter_01/1.1/isUnique.mjs
+++ javaScript/chapter_01/1.1/isUnique.mjs
@@ -1,6 +1,6 @@
 const isUniqueFuncs = {
 
-  //Straightforward solution in O(N) time.
+  //Straightforward solution in O(N^2) time.
   isUnique_01(str) {
     for (var i = 0; i < str.length; i++) {
         for (var j = i + 1; j < str.length; j++) {
@@ -11,22 +11,31 @@ const isUniqueFuncs = {
     }
     return true; 
   },
-  // Bit Char Code Solution
+  // Bit Char Code Solution O(N) time
   isUnique_02 (str, index = 'a'.charCodeAt() ) {
-    console.log("'a'.charCodeAt(): ", 'a'.charCodeAt()); 
-    let count = Number();
-    console.log("count: ", count);
+    let count = 0; 
     for(let k of [...str].map(c => c.charCodeAt() - index)) {
-      console.log("c.charCodeAt(): ", [...str].map(c => c.charCodeAt() - index)); 
+      // console.log("count: ", count);
+      // console.log("count bit: ", (count >>> 0).toString(2));
+      // console.log("k: ", k);
+      // console.log("k bit: ", (k%32 >>> 0).toString(2));
+      // console.log("c.charCodeAt(): ", [...str].map(c => c.charCodeAt() - index)); 
       const shift = 1 << k;
-      console.log("shift: ", shift); 
-      if(count & shift)
-          return false;
-      count |= shift;
+      // console.log("1 << k: ", (1 << k).toString(2))
+      // console.log("shift: ", shift); 
+      // console.log("shift bit: ", (shift >>> 0).toString(2)); 
+      // Bitwise And returns a 1 if both are in common  
+      // console.log("count & shift: ", count & shift)
+      if(count & shift) {
+        return false;
+      }
+      count |= shift; // count = count | shift
+      // console.log("-----------"); 
     }
     return true;
   }, 
   // Utilizes a mirror object to place 1's at the indices of the characters
+  // Similar to above solution withut bitwise operators 
   isUnique_03(str) {
     let mirror = {};
     for (let i = 0; i < str.length; i++) {
--- javaScript/chapter_01/1.1/runImports.js
+++ javaScript/chapter_01/1.1/runImports.js
@@ -2,5 +2,5 @@ import isUniqueFuncs from './isUnique.mjs';
 
 
 // var ans = isUniqueFuncs.isUnique_01("testme"); 
-var ans = isUniqueFuncs.isUnique_02("wedofiru", 0)
+var ans = isUniqueFuncs.isUnique_02("abcda", 0)
 console.log(ans)
